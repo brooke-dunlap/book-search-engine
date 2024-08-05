b059a0f0c8c2e1ffbc62f7eb86f40023f44de10c
Brooke <brookedunlap97@gmail.com>
2024-08-05 16:23:19 -0500

    Added schemas folder and set up typeDefs and resolvers to be further expanded upon. Updated the auth middleware function to work with the GraphQL API

---
+++ server/Schemas/resolvers.js
@@ -0,0 +1,19 @@
+const typeDefs = `
+    type User {
+    }
+
+    type Book {
+    }
+
+    type Auth {
+    }
+    
+    type Query {
+    }
+    
+    type Mutation {
+    }
+`;
+
+
+module.exports = typeDefs;
