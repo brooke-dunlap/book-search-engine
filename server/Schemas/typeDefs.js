51d04b02a9faf22290d5db223744081735f5fd64
Brooke <brookedunlap97@gmail.com>
2024-08-08 22:28:09 -0500

    created mutations and queries for the front end. created an apollo provider within app.jsx

--- server/Schemas/typeDefs.js
+++ server/Schemas/typeDefs.js
@@ -1,48 +1,50 @@
-const typeDefs = `
-    type User {
-        _id: ID!
-        username: String!
-        email: String!
-        password: String!
-        bookCount: Int
-        savedBooks [Book]
-    }
+const { gql } = require("apollo-server-express");
 
-    type Book {
-        bookId: String!
-        authors: [String]
-        description: String
-        title: String
-        image: String
-        link: String
-    }
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String!
+    email: String!
+    savedBooks: [Book]
+  }
 
-    input BookInput {
-        bookId: String!
-        authors: [String]
-        description: String
-        title: String
-        image: String
-        link: String
-    }
+  type Book {
+    bookId: String!
+    authors: [String]
+    description: String!
+    title: String!
+    image: String
+    link: String
+    rating: Int
+    review: String
+    readerStatus: Int
+  }
 
-    type Auth {
-        token: String!
-        user: User!
-    }
-    
-    type Query {
-        me: User
-    }
-    
-    type Mutation {
-        login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(book: bookInput!): User
-        removeBook(bookdId: String!): User
-    }
+  input BookInput {
+    bookId: String!
+    authors: [String]
+    description: String!
+    title: String!
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me(id: String, username: String): User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: String!): User
+  }
 `;
-
 
 module.exports = typeDefs;
 
